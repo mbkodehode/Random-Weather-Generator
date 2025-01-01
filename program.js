6f83a0299a2758ed24ed2f6e5220d71918f14539
mbkodehode <mb1811kodehode@gmail.com>
2025-01-01 23:11:23 +0100

    js done

--- program.js
+++ program.js
@@ -3,13 +3,56 @@ class Weather {
         this.temperature = 0;
         this.condition = "";
         this.cloudiness = 0;
+        this.windspeed = 0;
     }
     updateWeather() {
         this.temperature = Math.floor(Math.random() * 50) - 10;
+        this.cloudiness = Math.floor(Math.random() * 100);
+        this.windspeed = Math.floor(Math.random() * 100);
         if (this.temperature <= 0) {
             this.condition = ["snow"];
         } else {
-            this.condition = ["rain", "clear"];
+            let rand = Math.floor(Math.random() * 2);
+                if (rand <= 0.5) {
+                this.condition = ["rain",];
+            }else{
+                this.condition = ["clear"];
+            }
         }
     }
 }
+
+let interval = setInterval(() => {
+    let weather = new Weather();
+    weather.updateWeather();
+    document.getElementById("temperature-value").textContent = weather.temperature;
+    document.getElementById("condition-value").textContent = weather.condition;
+    document.getElementById("cloudiness-value").textContent = weather.cloudiness;
+    document.getElementById("windspeed-value").textContent = weather.windspeed;
+}, 3000);
+
+function createRainElement() {
+    const rain = document.createElement("div");
+    rain.classList.add("rain");
+    rain.innerText = "💧"
+    rain.style.left = `${Math.random() * 100}vw`
+    rain.style.animationDuration = `${Math.random() * 1 + 2}s`
+    document.getElementById("rain-animation").appendChild(rain);
+}
+
+function createSnowElement() {
+    const rain = document.createElement("div");
+    rain.classList.add("snow");
+    rain.innerText = "❄️"
+    rain.style.left = `${Math.random() * 100}vw`
+    rain.style.animationDuration = `${Math.random() * 1 + 2}s`
+    document.getElementById("snow-animation").appendChild(snow);
+}
+createRainElement();
+
+createSnowElement();
+updateWeather();
+setInterval(() => {
+    createRainElement();
+}, 10);
+
